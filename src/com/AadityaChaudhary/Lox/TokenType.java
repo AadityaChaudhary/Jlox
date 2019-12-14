55c58baff75585c6dceb548f2d1528dd03a67bc6
aadi <AadityaChaudhary@1360.ca>
2019-12-13 23:05:05 -0500

    initial commit

---
+++ src/com/AadityaChaudhary/Lox/TokenType.java
@@ -0,0 +1,23 @@
+package com.AadityaChaudhary.Lox;
+
+public enum TokenType {
+
+    // Single-character tokens
+    L_PAREN, R_PAREN, L_BRACE, R_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    // One or two character tokens
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+
+    //literals
+    IDENTIFIER, STRING, NUMBER,
+
+    //keywords
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    EOF
+}
